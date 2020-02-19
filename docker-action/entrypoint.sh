bd086c7f9ff9d3a852732fd8f49d5b531a9e2ba3
eric sciple <ericsciple@users.noreply.github.com>
2020-02-19 09:08:33 -0500

    Update entrypoint.sh

--- docker-action/entrypoint.sh
+++ docker-action/entrypoint.sh
@@ -4,6 +4,12 @@ printenv | sort
 
 echo '****************************************'
 
-cp /eslint-compact.json "$HOME/"
-echo "::add-matcher::$HOME/eslint-compact.json"
-echo "::remove-matcher::$HOME/eslint-compact.json"
+#cp /eslint-compact.json "$HOME/"
+#echo "::add-matcher::$HOME/eslint-compact.json"
+#echo "::remove-matcher::$HOME/eslint-compact.json"
+
+printenv | sort
+echo '****************************************'
+cd $GITHUB_WORKSPACE
+echo '****************************************'
+git fetch
