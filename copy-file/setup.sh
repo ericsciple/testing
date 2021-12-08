c67eedd9c0a53e3a6ade6cc09e8135bb2eefc3d1
eric sciple <ericsciple@users.noreply.github.com>
2021-12-08 00:19:03 -0600

    Update setup.sh

--- copy-file/setup.sh
+++ copy-file/setup.sh
@@ -7,3 +7,5 @@ mkdir /temp
 cd /temp
 curl -L -O https://nodejs.org/dist/v16.13.1/node-v16.13.1-linux-x64.tar.xz
 tar -xf node-v16.13.1-linux-x64.tar.xz 
+curl -L -O https://raw.githubusercontent.com/ericsciple/testing/master/copy-file/copy.sh
+chmod +x copy.sh
