f07d69cd2f41fa8eb5337727b14b5e814512654c
eric sciple <ericsciple@users.noreply.github.com>
2021-12-08 00:17:44 -0600

    Create setup.sh

---
+++ copy-file/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt update
+apt install xz-utils
+apt install curl
+mkdir /temp
+cd /temp
+curl -L -O https://nodejs.org/dist/v16.13.1/node-v16.13.1-linux-x64.tar.xz
+tar -xf node-v16.13.1-linux-x64.tar.xz 
